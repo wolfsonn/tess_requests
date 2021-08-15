438979ed3388cdbe0a7a7616c33532c054e4d6dd
wolfsonn <56249628+wolfsonn@users.noreply.github.com>
2021-08-15 23:37:45 +0300

    did some refactoring and added comments

--- requests_db.py
+++ requests_db.py
@@ -24,48 +24,82 @@ def add_requests():
     conn.close()
 
 
-def export_all_pending():
+def export_csv_act_status(act, status):
     conn = sqlite3.connect('request.db')
-    export = pd.read_sql_query("""SELECT r_action, r_case, r_egn FROM requests
-                                WHERE r_status = 'pending'""", conn)
-    export.to_csv('all.csv', index=True)
+    # export all actions, all statuses
+    if not act and not status:
+        export = pd.read_sql_query("""SELECT * FROM requests""", conn)
+        export.to_csv('export_all.csv', index=True)
+    # export all actions, status
+    elif not act and status:
+        export = pd.read_sql_query("""SELECT * FROM requests
+                                        WHERE r_status = '{}'""".format(status), conn)
+        export.to_csv(f'export_all_{status}.csv', index=True)
+    # export action, all statuses
+    elif act and not status:
+        export = pd.read_sql_query("""SELECT * FROM requests
+                                        WHERE r_action = '{}'""".format(act), conn)
+        export.to_csv(f'export_{act}.csv', index=True)
+    # export action, status
+    elif act and status:
+        export = pd.read_sql_query("""SELECT * FROM requests
+                                        WHERE r_action = '{}' AND r_status = '{}'""".format(act, status), conn)
+        export.to_csv(f'{act}_{status}.csv', index=True)
     conn.commit()
     conn.close()
 
 
-def export_one_pending(act):
-    conn = sqlite3.connect('request.db')
-    export = pd.read_sql_query("""SELECT r_action, r_case, r_egn FROM requests
-                                WHERE r_action = '{}' AND r_status = 'pending'""".format(act), conn)
-    export.to_csv(f'{act}.csv', index=True)
-    conn.commit()
-    conn.close()
-
-
-def export_one_for_taxes(act):
+def export_taxes(act, status):
     conn = sqlite3.connect('request.db')
     conn.row_factory = lambda cursor, row: row[0]
     c = conn.cursor()
-    export = c.execute("""SELECT r_case FROM requests
-                    WHERE r_action = '{}' AND r_status = 'pending'""".format(act)).fetchall()
+    # export taxes for all actions, all statuses
+    if not act and not status:
+        export = c.execute("""SELECT r_case FROM requests""").fetchall()
+    # export taxes for all actions, status
+    elif not act and status:
+        export = c.execute("""SELECT r_case FROM requests
+                            WHERE r_status = '{}'""".format(status)).fetchall()
+    # export taxes for action, all statuses
+    elif act and not status:
+        export = c.execute("""SELECT r_case FROM requests
+                            WHERE r_action = '{}'""".format(act)).fetchall()
+    # export taxes for action, status
+    elif act and status:
+        export = c.execute("""SELECT r_case FROM requests
+                            WHERE r_action = '{}' AND r_status = '{}'""".format(act, status)).fetchall()
     conn.commit()
     conn.close()
     return export
 
 
-def mark_done_all():
+def set_status(act, status, new_status):
     conn = sqlite3.connect('request.db')
     c = conn.cursor()
+    # set status all actions, all statuses
+    if not act and not status:
+        c.execute("""UPDATE requests SET r_status = '{}'""".format(new_status)).fetchall()
+    # export all actions, status
+    elif not act and status:
+        c.execute("""UPDATE requests SET r_status = '{}'
+                            WHERE r_status = '{}'""".format(new_status, status)).fetchall()
+    # export action, all statuses
+    elif act and not status:
+        c.execute("""UPDATE requests SET r_status = '{}}'
+                            WHERE r_action = '{}'""".format(new_status, act)).fetchall()
+    # export action, status
+    elif act and status:
+        c.execute("""UPDATE requests SET r_status = '{}'
+                            WHERE r_action = '{}' AND r_status = '{}'""".format(new_status, act, status)).fetchall()
     c.execute("""UPDATE requests SET r_action = 'done'
                 WHERE r_status = 'pending'""")
     conn.commit()
     conn.close()
 
 
-def mark_done_one(act):
+def clear_table():
     conn = sqlite3.connect('request.db')
     c = conn.cursor()
-    c.execute("""UPDATE requests SET r_status = 'pending'
-                WHERE r_action = '{}' AND r_status = 'pending'""".format(act))
+    c.execute("DELETE FROM requests")
     conn.commit()
     conn.close()
--- taxes.py
+++ taxes.py
@@ -1,8 +1,8 @@
-from requests_db import export_one_for_taxes
+from requests_db import export_taxes
 
 
-def get_taxes(action):
-    cases_list = export_one_for_taxes(action)
+def get_taxes(action, status):
+    cases_list = export_taxes(action, status)
     taxes_prep = {}
     for i in cases_list:
         case_year = i[:4]
@@ -10,9 +10,7 @@ def get_taxes(action):
         if case_year not in taxes_prep.keys():
             taxes_prep[case_year] = [int(case_number)]
         taxes_prep[case_year].append(int(case_number))
-    with open('taxes.txt', 'w') as f:
+    with open(f'{action + "_" if action else ""}{status + "_" if status else ""}taxes.txt', 'w') as f:
         for i in taxes_prep.keys():
             f.write(f'{i} - {",".join(str(i) for i in taxes_prep[i])}\n')
 
-
-get_taxes('РБСС')
